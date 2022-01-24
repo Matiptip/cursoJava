5b66d8047fbd2844b6bd3ca6833fda09145fcc3d
Matiptip <98347856+Matiptip@users.noreply.github.com>
2022-01-24 15:18:41 -0300

    Add files via upload

---
+++ unidad1/Script.js
@@ -0,0 +1,32 @@
+/*Variables*/
+let numero = (prompt("Introduce un numero"));
+let primo = true;
+
+/*Exepciones*/
+if (numero === 1 || numero === 0) {
+    alert("0 y 1 no corresponden a numeros primos por definicion.");
+}
+
+
+/*Ciclo: definicion de numero primo (Solo funciona con positivos)*/
+else if (numero > 1) {
+
+    for (let i = 2; i < numero; i++) {
+        if (numero % i == 0) {
+            primo = false;
+            break;
+        }
+    }
+
+    
+/*salidas */
+    if (primo) {
+        alert(`${numero} es un numero primo`);
+    } else {
+        alert(`${numero} no es un numero primo`);
+    }
+}
+
+else {
+    alert("Por el momento, solo funciono con numeros positivos.");
+}
